5493bdad68000d56d8b3b8f774453ed76db84452
Miqueias Ayron <miqueiasamf@al.insper.edu.br>
2026-08-26 00:50:53 -0300

    feat: classe Produto implementada

--- exercicio2/src/main/java/org/example/Produto.java
+++ exercicio2/src/main/java/org/example/Produto.java
@@ -1,4 +1,23 @@
 package org.example;
 
 public class Produto {
+    private int codigo;
+    private String nome;
+    private double preco;
+    private Categoria categoria;
+
+    public Produto(int codigo, String nome, double preco, Categoria categoria){
+        this.codigo = codigo;
+        this.nome = nome;
+        this.preco = preco;
+        this.categoria = categoria;
+    }
+
+    public void aplicarDesconto(double percentual){
+        System.out.println("PRECO COM DESCONTO " + this.preco*(1 - percentual/100));
+    }
+
+    public void aumentarPreco(double percentual){
+        System.out.println("NOVO PRECO " + this.preco*(1 + percentual/100));
+    }
 }
